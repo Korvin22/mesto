948315f47fb4562f72d52a1260826e1d2fbf1526
Dmitrii Gudemenko <dmitry.gudemenko@yandex.ru>
2022-06-30 06:59:24 +0300

    popup+js_constants

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,12 @@
 console.log('hello, world!');
+
+const popupElement = document.querySelector('.popup');
+const popupCloseButtonElement = popupElement.querySelector('.popup__button-close');
+const popupOpenButtonElement = document.querySelector('.profile__open-popup');
+
+const togglePopupVisibility = function () {
+
+  console.log('togglePopupVisibility');
+}
+
+togglePopupVisibility();
