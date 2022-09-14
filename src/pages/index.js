558c32c25d2a23175ada3185506f70b696712ea2
Dmitrii Gudemenko <dmitry.gudemenko@yandex.ru>
2022-09-14 18:16:27 +0300

    fix: after 1 review

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,11 @@
 import "../pages/index.css";
-import { selectors, formAddEdit, initialCards, addSpinner, removeSpinner } from "../utils/constants.js";
+import {
+  selectors,
+  formAddEdit,
+  initialCards,
+  addSpinner,
+  removeSpinner,
+} from "../utils/constants.js";
 import { FormValidator } from "../components/FormValidator.js";
 import { Card } from "../components/Card.js";
 import { Section } from "../components/section.js";
@@ -26,7 +32,6 @@ buttonOpenPopupAvatar.addEventListener("click", () => {
 });
 
 const classPopupEdit = new PopupWithForm(".popup-edit", (formData) => {
-
   classUserInfo.setUserInfo({
     name: formData.name,
     dedication: formData.dedication,
@@ -41,11 +46,10 @@ const classUserInfo = new UserInfo(
   ".profile__image"
 );
 const classPopupAvatar = new PopupWithForm(".popup-avatar", (formData) => {
+  console.log(formData);
   api.changeAvatar(formData).then((res) => {
-    addSpinner(document.querySelector('.popup__button-save'));
+    console.log(formData);
     return classUserInfo.setAvatar(formData.reference);
-  }).finally(()=>{
-    removeSpinner(document.querySelector('.popup__button-save'));
   });
   classPopupAvatar.closePopup();
 });
